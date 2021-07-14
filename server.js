063fbb85d2fd12a2cde4f21f5ccd13d6bfd9da92
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-07-14 14:17:20 -0500

    Updated mongoose.connect to use useUnifiedTopology

--- server.js
+++ server.js
@@ -28,13 +28,14 @@ app.use(express.static("public"));
 
 mongoose.connect(mongoDbConnStr, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log("Connected using " + mongoDbConnStr);
+  console.log("Connected to = " + mongoDbConnStr);
   console.log(`App running on port ${PORT}!`);
 });
