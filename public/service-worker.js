7d90ed4b06420f2fc26936e5ed03b206ad4198a1
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-07-15 14:06:00 -0500

    Added on service worker install event

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,12 +14,14 @@ const cacheFiles =
 self.addEventListener("install", (event) => {
 
     // Caches site static files
+    console.log("Caching static files");
     event.waitUntil(
         caches.open(staticCacheName)
         .then((cache) => cache.addAll(cacheFiles))
     );
 
     // Cache the /api/transaction fetch from MongoDB
+    console.log("Caching transactions (API)");
     event.waitUntil(
         caches.open(dataCacheName)
         .then((cache) => cache.add("/api/transaction"))
@@ -27,12 +29,22 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-    console.log("In 'Activate'");
-    console.log("****************************");
-    console.log(caches.keys());
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    
+    // Fetches the cache storage key names
+    console.log("Activating");
     event.waitUntil(
-        console.log(caches.keys())
+        caches.keys()
+        .then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== staticCacheName && key !== dataCacheName) {
+                        console.log("Removing old cache data", "[", key, "]");
+                        return caches.delete(key);
+                    }
+                })
+            )
+        })
     );
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    
+    self.clients.claim();
 });
